a5b5bf79805c1f8ac31edc00c14f0af620b7d160
tredeneo <tredeneo@hotmail.com>
2021-08-05 20:40:57 -0300

    refatorado redes, e n funcionando com imagem

--- redes/web_server_socket/redes.py
+++ redes/web_server_socket/redes.py
@@ -1,40 +1,63 @@
 import threading
 import socket
+import os
 from pathlib import Path
 
 
 def tratar(cliente, endereco):
+    info = cliente.recv(1024).decode()
+    tipo_arquivo = info.split()[1].split(".")[1]
+    nome_arquivo = "." + info.split()[1]
     try:
-        info = cliente.recv(1024).decode()
-        print(info)
-        nome = "." + info.split()[1]
-        arquivo = open(nome, "r")
-        saida = arquivo.read()
-        print(arquivo)
-        arquivo.close()
-        resposta = "HTTP/1.1 200 OK \n"
-        resposta += "Content-type: text/html\n"
-        cliente.send(resposta.encode())
-        cliente.sendall(saida.encode())
-        # for i in range(len(saida)):
-        #     cliente.send(saida[i].encode())
-    except IsADirectoryError:
-        print(f"{IsADirectoryError}")
+        if tipo_arquivo == "jpg":
+            arquivo = open(nome_arquivo, "rb")
+            saida = arquivo.read()
+            resposta = "HTTP/1.1 200 OK \n"
+            resposta += "Content-type: image/jpeg\n"
+            cliente.send(resposta.encode())
+            cliente.send(saida)
+            # cliente.sendfile(arquivo)
+            # cliente.send(f"Content-Length:{len(saida)}".encode())
+            # for i in range(len(saida)):
+            #     cliente.send(saida[i])
+            # while saida:
+            #     print("ainda")
+            #     cliente.send(saida)
+            #     saida = arquivo.read(1024)
+        else:
+            mandar_html(cliente, nome_arquivo, "200")
     except FileNotFoundError:
         print("arquivo não encontrado")
-        resposta = "HTTP/1.1 404 OK \n"
-        resposta += "Content-type: text/html\n"
+        mandar_html(cliente, "./erro.html", "404")
+
+    except Exception as ex:
+        print(f"thread\n{ex}")
+    cliente.close()
+
+
+def mandar_html(cliente, nome, codigo):
+    arquivo = open(nome, "r")
+    saida = arquivo.read()
+    resposta = f"HTTP/1.1 {codigo} OK \n"
+    resposta += "Content-type: text/html\n"
+    cliente.send(resposta.encode())
+    cliente.sendall(saida.encode())
     cliente.close()
 
 
 if __name__ == "__main__":
+    porta = 8094
+    print(f"porta: {porta}\n")
     socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_servidor.bind(("0.0.0.0", 8089))
+    socket_servidor.bind(("0.0.0.0", porta))
     socket_servidor.listen(5)
     try:
         while True:
             (cliente, endereco) = socket_servidor.accept()
             thread = threading.Thread(target=tratar(cliente, endereco))
             thread.start()
+    except Exception as ex:
+        print(f"main\n{ex}")
     except KeyboardInterrupt:
-        socket_servidor.close()
+        print("cabo")
+    socket_servidor.close()
